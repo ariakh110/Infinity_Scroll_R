e93970103a7e12afdc52a84df43259672a77e0ab
aria khayer <ariakh110@gmail.com>
2022-05-05 11:03:25 +0430

    prepare this app for react query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import _ from "lodash";
+import { useInfiniteQuery } from "react-query";
 //components
 import Uesr from "./User";
 //styles
@@ -8,25 +9,64 @@ import { Content, Loading } from "./App.styles";
 import { getUsers } from "./API";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, fetchNextPage, hasNextPage, error } =
+    useInfiniteQuery("users", getUsers, {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.page < lastPage.total_pages) {
+          return { page: lastPage.page + 1 };
+        }
+        return false;
+      },
+      refetchOnWindowFocus: false,
+    });
+  // const handleScroll = (e) => {
+  //   const { scrollHeight, scrollTop, clientHeight } = e.target;
+  //   if (scrollHeight - scrollTop === clientHeight) {
+  //     setPage(page + 1);
+  //   }
+  // };
+  console.log(`has :${hasNextPage}`);
 
-  useEffect(() => {
-    const loadUsers = async () => {
-      setLoading(true);
-      const newUsers = await getUsers(page);
-      setUsers((prev) => [...prev, ...newUsers]);
-      setLoading(false);
-    };
-    loadUsers();
-  }, [page]);
+  //for react query
+  const handleScrollRQ = (e) => {
+    const { scrollHeight, scrollTop, clientHeight } = e.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      fetchNextPage();
+    }
+  };
+
+  // useEffect(() => {
+  //   const loadUsers = async () => {
+  //     setLoading(true);
+  //     const newUsers = await getUsers(page);
+  //     setUsers((prev) => [...prev, ...newUsers]);
+  //     setLoading(false);
+  //   };
+  //   loadUsers();
+  // }, [page]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (isLoading) {
+    return <Loading>Loading...</Loading>;
+  }
+  console.log(data);
   return (
     <div className="App">
-      {loading && <Loading>Loading...</Loading>}
-      <Content>
-        {users && _.map(users, (user) => <Uesr key={user.cell} user={user} />)}
-      </Content>
+      {/* {loading && <Loading>Loading...</Loading>} */}
+      {
+        <Content onScroll={handleScrollRQ}>
+          {data.pages &&
+            _.map(data.pages, (page) =>
+              _.map(page, (user) => <Uesr key={user.cell} user={user} />)
+            )}
+        </Content>
+      }
+      {isFetching && <Loading>Loading...</Loading>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
